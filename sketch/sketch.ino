c999fb1e2867d4ff328f708943a041487ca55846
Phillip L <p410n3@gmail.com>
2017-03-07 12:56:13 +0100

    UltraSonic now works!
    Only some offsets are not proper.

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -3,25 +3,27 @@
 
 /*
     Steuerung :
-    1 -> Rückwärts
-    2 -> Vorwärts
-    3 -> Rechts
-    4 -> Links
-    0 -> STOP
-    8 -> Schneller
-    9 -> Langsamer 
-
-    wenn aus folgender Perspektive betrachtet : Von Hinten (ArduinoBoard) nach Vorne (Batt.Pack)
-    MOTOR_A => Rechte Seite
-    MOTOR_B => Linke Seite
+ 1 -> Rückwärts
+ 2 -> Vorwärts
+ 3 -> Rechts
+ 4 -> Links
+ 0 -> STOP
+ 8 -> Schneller
+ 9 -> Langsamer 
+ 
+ wenn aus folgender Perspektive betrachtet : Von Hinten (ArduinoBoard) nach Vorne (Batt.Pack)
+ MOTOR_A => Rechte Seite
+ MOTOR_B => Linke Seite
  */
 
 SoftwareSerial BT(10,11); //[BT-Adapter TX 10, RX 11]
 
-int aPhase[2] = {2, 3};    //[MotorA]Pin APHASE des Motortreibers an Digital Pin 2,3 vom Arduino
+int aPhase[2] = {
+  2, 3};    //[MotorA]Pin APHASE des Motortreibers an Digital Pin 2,3 vom Arduino
 int aEnable = 6;   //[MotorA]Pin AENABLE des Motortreibers an Digital Pin 6 vom Arduino
 
-int bPhase[2] = {4, 5}; //[MotorB]Pin BPHASE des Motortreibers an Digital Pin 4,5 vom Arduino
+int bPhase[2] = {
+  4, 5}; //[MotorB]Pin BPHASE des Motortreibers an Digital Pin 4,5 vom Arduino
 int bEnable = 7; //[MotorB]Pin BENABLE des Motortreibers an Digital Pin 7 vom Arduino
 
 int bt_input;
@@ -37,11 +39,19 @@ boolean steht = true;
 Motor MOTOR_A(aEnable, aPhase[0], aPhase[1]);
 Motor MOTOR_B(bEnable, bPhase[0], bPhase[1]);
 
-//ultraschallsensor
+//function
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  //Totally not stolen code
+  return microseconds / 29 / 2;
+}
 
 //Geradeaus
-int trigger1=12;
-int echo1=13;
+int trigger1=29;
+int echo1=28;
 long dauer1=0;
 long entfernung1=0;
 
@@ -67,7 +77,7 @@ void setup()
   //Bluetooth initialisieren
   BT.begin(9600);
   BT.println("Bluetooth initialized");
-  
+
   //Ultraschall sensor
   Serial.begin (9600);
   pinMode(trigger1, OUTPUT);
@@ -77,10 +87,11 @@ void setup()
   pinMode(trigger2, OUTPUT);
   pinMode(echo2, INPUT);
 
-
   Serial.begin (9600);
   pinMode(trigger3, OUTPUT);
   pinMode(echo3, INPUT);
+
+
 
 }
 
@@ -91,60 +102,60 @@ void loop()
     bt_input = BT.read();
     BT.println("Bluetooth Rückmeldung : Hat folgenden Wert empfangen: ");
     BT.println(bt_input);
-   
+
     //------------------------- Set Speed --------------------------
     /* Currently not working
-    
-    if(bt_input == '8')
-    {
-      MOTOR_B.Set_Speed(MOTOR_A.Give_Speed());
-      if((MOTOR_A.Give_Speed()) <= 219 )
-      {
-        MOTOR_A.Set_Speed((MOTOR_A.Give_Speed() + 35));
-        MOTOR_B.Set_Speed((MOTOR_B.Give_Speed() + 35));
-        BT.print("Speed +35   ==>   ");BT.println(MOTOR_A.Give_Speed());
-        lastSpeedA = MOTOR_A.Give_Speed();
-        lastSpeedB = MOTOR_B.Give_Speed();
-      }
-      else
-      {
-        MOTOR_A.Set_Speed(254);
-        MOTOR_B.Set_Speed(254);
-        BT.print("Speed MAX   ==>   ");BT.println(MOTOR_A.Give_Speed());
-        lastSpeedA = MOTOR_A.Give_Speed();
-        lastSpeedB = MOTOR_B.Give_Speed();
-      }
-    }
-    else
-    {
-      if(bt_input == '9')
-      {
-        MOTOR_B.Set_Speed(MOTOR_A.Give_Speed());
-        if( (MOTOR_A.Give_Speed()) >= 55)
-        {
-          MOTOR_A.Set_Speed((MOTOR_A.Give_Speed() - 35));
-          MOTOR_B.Set_Speed((MOTOR_B.Give_Speed() - 35));
-          BT.print("Speed -35   ==>   ");BT.println(MOTOR_A.Give_Speed());
-          lastSpeedA = MOTOR_A.Give_Speed();
-          lastSpeedB = MOTOR_B.Give_Speed();
-        }
-        else
-        {
-          MOTOR_A.Set_Speed(25);
-          MOTOR_B.Set_Speed(25);
-          BT.print("Speed MIN   ==>   ");BT.println(MOTOR_A.Give_Speed());
-          lastSpeedA = MOTOR_A.Give_Speed();
-          lastSpeedB = MOTOR_B.Give_Speed();
-        }
-      }
-    }
-    //--------------------------------------------------------------
-    */    
+     
+     if(bt_input == '8')
+     {
+     MOTOR_B.Set_Speed(MOTOR_A.Give_Speed());
+     if((MOTOR_A.Give_Speed()) <= 219 )
+     {
+     MOTOR_A.Set_Speed((MOTOR_A.Give_Speed() + 35));
+     MOTOR_B.Set_Speed((MOTOR_B.Give_Speed() + 35));
+     BT.print("Speed +35   ==>   ");BT.println(MOTOR_A.Give_Speed());
+     lastSpeedA = MOTOR_A.Give_Speed();
+     lastSpeedB = MOTOR_B.Give_Speed();
+     }
+     else
+     {
+     MOTOR_A.Set_Speed(254);
+     MOTOR_B.Set_Speed(254);
+     BT.print("Speed MAX   ==>   ");BT.println(MOTOR_A.Give_Speed());
+     lastSpeedA = MOTOR_A.Give_Speed();
+     lastSpeedB = MOTOR_B.Give_Speed();
+     }
+     }
+     else
+     {
+     if(bt_input == '9')
+     {
+     MOTOR_B.Set_Speed(MOTOR_A.Give_Speed());
+     if( (MOTOR_A.Give_Speed()) >= 55)
+     {
+     MOTOR_A.Set_Speed((MOTOR_A.Give_Speed() - 35));
+     MOTOR_B.Set_Speed((MOTOR_B.Give_Speed() - 35));
+     BT.print("Speed -35   ==>   ");BT.println(MOTOR_A.Give_Speed());
+     lastSpeedA = MOTOR_A.Give_Speed();
+     lastSpeedB = MOTOR_B.Give_Speed();
+     }
+     else
+     {
+     MOTOR_A.Set_Speed(25);
+     MOTOR_B.Set_Speed(25);
+     BT.print("Speed MIN   ==>   ");BT.println(MOTOR_A.Give_Speed());
+     lastSpeedA = MOTOR_A.Give_Speed();
+     lastSpeedB = MOTOR_B.Give_Speed();
+     }
+     }
+     }
+     //--------------------------------------------------------------
+     */
     if(bt_input != '3' && bt_input != '4')
     {
       MOTOR_B.Set_Speed(MOTOR_A.Give_Speed());
     }
-    
+
     if(bt_input == '1')
     {
       MOTOR_A.Fahren(1);
@@ -226,143 +237,129 @@ void loop()
             }
             else
             {
-            
-             if (bt_input == '7')
-            {
-                  BT.println("blub");
-                digitalWrite(trigger1, LOW);
-                delay(5);
-                digitalWrite(trigger1, HIGH);
-                delay(10);
-                digitalWrite(trigger1, LOW);
-                dauer1 = pulseIn(echo1, HIGH);
-                entfernung1 = (dauer1/2) / 29.1;
-                
-                digitalWrite(trigger2, LOW);
-                delay(5);
-                digitalWrite(trigger2, HIGH);
-                delay(10);
-                digitalWrite(trigger2, LOW);
-                dauer2 = pulseIn(echo2, HIGH);
-                entfernung2 = (dauer2/2) / 29.1;
-                
-                digitalWrite(trigger3, LOW);
-                delay(5);
-                digitalWrite(trigger3, HIGH);
-                delay(10);
-                digitalWrite(trigger3, LOW);
-                dauer3 = pulseIn(echo3, HIGH);
-                entfernung3 = (dauer3/2) / 29.1;
-                
-                 MOTOR_A.Fahren(2);
-                 MOTOR_B.Fahren(2);
-                 lastPhaseA=2;
-                 lastPhaseB=2;
-                 lastSpeedA = MOTOR_A.Give_Speed();
-                 lastSpeedB = MOTOR_B.Give_Speed();
-                 steht = false;
-                            
-              if (entfernung1 <= 15)
-                {
-                  BT.println("Stoppe");
-                if (entfernung2 >=15)
-                  {
-                    BT.println("fahre/drehe nach links");
-                      MOTOR_A.Fahren(2,127);
-                      MOTOR_B.Fahren(1,127);
-                      lastPhaseA=2;
-                      lastPhaseB=1;
-                      lastSpeedA = 127;
-                      lastSpeedB = 127;
-                     
-                      digitalWrite(trigger2, LOW);
-                      delay(5);
-                      digitalWrite(trigger2, HIGH);
-                      delay(10);
-                      digitalWrite(trigger2, LOW);
-                      dauer2 = pulseIn(echo2, HIGH);
-                      entfernung2 = (dauer2/2) / 29.1;
-                  }
-           
-                    if (entfernung3 >=15)
-                    {
-                      BT.println("fahre/drehe nach rechts");
-                        MOTOR_A.Fahren(1,127);
-                        MOTOR_B.Fahren(2,127);
-                        lastPhaseA=1;
-                        lastPhaseB=2;
-                        lastSpeedA = 127;
-                        lastSpeedB = 127;
-                        
-                        digitalWrite(trigger3, LOW);
-                        delay(5);
-                        digitalWrite(trigger3, HIGH);
-                       delay(10);
-                        digitalWrite(trigger3, LOW);
-                        dauer3 = pulseIn(echo3, HIGH);
-                        entfernung3 = (dauer3/2) / 29.1;
-                    }
-                     if(entfernung3 <=15 & entfernung2 <=15)
-                    {
-                     MOTOR_A.Fahren(1);
-                     MOTOR_B.Fahren(1);
-                     lastPhaseA = 1;
-                     lastPhaseB = 1;
-                     lastSpeedA = MOTOR_A.Give_Speed();
-                     lastSpeedB = MOTOR_B.Give_Speed();
-                     steht = false;
-                       
-                       digitalWrite(trigger2, LOW);
-                       delay(5);
-                       digitalWrite(trigger2, HIGH);
-                       delay(10);
-                       digitalWrite(trigger2, LOW);
-                       dauer2 = pulseIn(echo2, HIGH);
-                       entfernung2 = (dauer2/2) / 29.1;
-                        
-                        digitalWrite(trigger3, LOW);
-                        delay(5);
-                        digitalWrite(trigger3, HIGH);
-                        delay(10);
-                        digitalWrite(trigger3, LOW);
-                        dauer3 = pulseIn(echo3, HIGH);
-                        entfernung3 = (dauer3/2) / 29.1;
-                    }
-                    
-                    digitalWrite(trigger1, LOW);
-                    delay(5);
-                    digitalWrite(trigger1, HIGH);
-                    delay(10);
-                    digitalWrite(trigger1, LOW);
-                    dauer1 = pulseIn(echo1, HIGH);
-                    entfernung1 = (dauer1/2) / 29.1;
-                }
+              //Ab hier ist Ultraschallsensor
+              if (bt_input == '7')
+              {
+
+                do
+                {             
+                  //INIT US-Sensor
+                  digitalWrite(trigger1, LOW);
+                  //delayMicroseconds(2);
+                  digitalWrite(trigger1, HIGH);
+                  //delayMicroseconds(10);
+                  //dauer1 = pulseIn(echo1, HIGH);
+                  digitalWrite(trigger1, LOW);
+
+                  dauer1 = pulseIn(echo1, HIGH);
+                  entfernung1 = microsecondsToCentimeters(dauer1);
+
+
+                  digitalWrite(trigger2, LOW);
+                  //delayMicroseconds(2);
+                  digitalWrite(trigger2, HIGH);
+                  //delayMicroseconds(10);
+                  //dauer2 = pulseIn(echo2, HIGH);
+                  digitalWrite(trigger2, LOW);
+
+                  dauer2 = pulseIn(echo2, HIGH);
+                  entfernung2 = microsecondsToCentimeters(dauer2);
+
+                  digitalWrite(trigger3, LOW);
+                  //delayMicroseconds(2);
+                  digitalWrite(trigger3, HIGH);
+                  //delayMicroseconds(10);
+                  //dauer3 = pulseIn(echo3, HIGH);
+                  digitalWrite(trigger3, LOW);
                   
-                    if (entfernung1 <= 15)
-                    {
-                      if (entfernung2 >=15)
-                      {
-                        BT.println("fahre/drehe nach links");
-                      }
-                      else
-                      {
-                        if (entfernung3 >=15)
-                        {
-                          BT.println("fahre/drehe nach rechts");
-                        }
-                        else
-                        {
-                          BT.println("fahre rückwärts");
-                        }
-                      }
-                      
-                    }  
-                 }  
-                delay(3000);
+                  dauer3 = pulseIn(echo3, HIGH);
+                  entfernung3 = microsecondsToCentimeters(dauer3);
+
+                  //DBG
+                  /*
+                   BT.println("ENTFERNUNGEN: ");
+                   
+                   BT.println("MITTE: ");
+                   //mitte
+                   BT.println(entfernung1);
+                   
+                   BT.println("LINKS: ");
+                   //links
+                   BT.println(entfernung2);
+                   
+                   BT.println("RECHTS: ");
+                   //rechts                  
+                   BT.println(entfernung3);
+                   */
+
+
+                  //links < 10
+                  if(entfernung2 < 10)
+                  {
+                    MOTOR_A.Fahren(2,150);
+                    MOTOR_B.Fahren(1,150);
+                    lastPhaseA=2;
+                    lastPhaseB=1;
+                    lastSpeedA = 150;
+                    lastSpeedB = 150;
+                  }
+                  //rechts < 10
+                  else if(entfernung3 < 10)
+                  {
+                    MOTOR_A.Fahren(1,150);
+                    MOTOR_B.Fahren(2,150);
+                    lastPhaseA=1;
+                    lastPhaseB=2;
+                    lastSpeedA = 150;
+                    lastSpeedB = 150;
+                  }
+                  //mitte OK
+                  else if (entfernung1 > 10)
+                  {
+                    
+                    MOTOR_A.Fahren(2, 150);
+                    MOTOR_B.Fahren(2, 150);
+                    lastPhaseA=2;
+                    lastPhaseB=2;
+                    lastSpeedA = MOTOR_A.Give_Speed();
+                    lastSpeedB = MOTOR_B.Give_Speed();
+                    lastSpeedA = 150;
+                    lastSpeedB = 150;
+                  }
+                  //rückwärts
+                  else if(entfernung1 < 10)
+                  {
+                    MOTOR_A.Fahren(1, 150);
+                    MOTOR_B.Fahren(1, 150);
+                    lastPhaseA=1;
+                    lastPhaseB=1;
+                    lastSpeedA = MOTOR_A.Give_Speed();
+                    lastSpeedB = MOTOR_B.Give_Speed();
+                    lastSpeedA = 150;
+                    lastSpeedB = 150;
+                    
+                  }  
+                  
+                 
+                  
+                  //Only reads bt inout when soemthing is send. Maybe
+                  if(BT.available() > 0)
+                  {
+                    bt_input = BT.read();
+                  } 
+                }
+                while(bt_input == '7');
+              } 
             }
           }
         }
       }
-    } 
-  }  
-}
+    }
+  }
+} 
+
+
+
+
+
+
